13809e1bce9dd12eba2787adb23c79051eec008a
Drew Hill <andrew.hill@e-farmcredit.com>
2021-03-04 10:37:19 -0500

    Move styles

--- src/Hero/InfoCard/index.jsx
+++ src/Hero/InfoCard/index.jsx
@@ -8,13 +8,13 @@ function InfoCard() {
     console.log("get in touch");
   }
   return (
-    <div className="hero-content__info-card">
+    <div className="info-card">
       <div className="info-card__profile-image-container">
         <img src={profileImage} alt="profile" />
       </div>
       <h2>Glad to see you!</h2>
       <p>
-        I am a <Age birthday="1995-01-26"></Age> year old web developer located
+        I am a <Age birthday="1995-01-26" /> year old web developer located
         in Louisville, Kentucky. I have a passion for working with new
         technologies and take any opportunity I can get to lead an effort to
         adopt the latest and greatest.
